c76f7e1dc0b60f9937bf6f83da71759b202ac115
joymozumder <joy.mozumder.1003@gmail.com>
2022-09-29 10:31:12 +0600

    first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,35 @@
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Product.css';
+
+const Product = ({ handleAddToCart, product }) => {
+    // const { handleAddToCart, product } = props;
+    const {img, name, seller, price, ratings} = product;
+    return (
+      <div className="product">
+        <img src={img} alt=""></img>
+        <div className="product-info">
+          <p className="product-name">{name}</p>
+          <p>Price: ${price}</p>
+          <p>
+            <small>Seller: {seller}</small>
+          </p>
+          <p>
+            <small>Ratings: {ratings} stars</small>
+          </p>
+        </div>
+        <button
+          onClick={() => {
+            handleAddToCart(product);
+          }}
+          className="btn-cart"
+        >
+          <p>Add to cart </p>
+          <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+        </button>
+      </div>
+    );
+};
+
+export default Product;
