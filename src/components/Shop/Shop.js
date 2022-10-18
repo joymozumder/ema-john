75ffd9ef2f714b04166ade20049f4fc9ad009c59
joymozumder <joy.mozumder.1003@gmail.com>
2022-10-19 01:20:55 +0600

    local storage added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -6,13 +7,47 @@ const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
     useEffect(()=>{
+        // console.log('Products load before fetch')
         fetch('products.json')
         .then(res=> res.json())
-        .then(data => setProducts(data))
+        .then(data => {
+            setProducts(data);
+            // console.log("Products loaded"); 
+        })
     },[]);
-    const handleAddToCart = (product) => {
-      const newCart = [...cart, product];
-      setCart(newCart);
+
+    
+
+    useEffect(()=>{
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for(const id in storedCart){
+            const addedProduct  = products.find(product=> product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+
+        }
+        setCart(savedCart);
+    },[products])
+
+    const handleAddToCart = (selectedProduct) => {
+        let newCart = [];
+        const exists = cart.find(product=>product.id===selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+        
+        setCart(newCart);
+        addToDb(selectedProduct.id)
     };
     return (
         <div className='shop-container'>
